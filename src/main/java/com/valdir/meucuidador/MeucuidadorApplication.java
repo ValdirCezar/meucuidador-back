eace6b0407b44b2e33e7e76964ecab948e06772e
Valdir Cezar <valdircezar312@gmail.com>
2021-09-14 21:16:38 -0300

    chore: projeto iniciado

---
+++ src/main/java/com/valdir/meucuidador/MeucuidadorApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.meucuidador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucuidadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeucuidadorApplication.class, args);
+    }
+
+}
