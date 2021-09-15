e63e77643f72c1346cebb79d7064e300eeb93e47
Valdir Cezar <valdircezar312@gmail.com>
2021-09-15 12:22:11 -0300

    feat: classe abstrata usuario dto criada

---
+++ src/main/java/com/valdir/meucuidador/domain/dto/UsuarioDTO.java
@@ -0,0 +1,46 @@
+package com.valdir.meucuidador.domain.dto;
+
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class UsuarioDTO {
+
+    protected Integer id;
+
+    @NotEmpty(message = "Campo NOME é requerido")
+    @Size(min = 3, max = 99, message = "O campo NOME deve ter entre 3 e 99 caracteres")
+    protected String nome;
+
+    @NotEmpty(message = "Campo CPF é requerido")
+    @CPF
+    protected String cpf;
+
+    @NotEmpty(message = "Campo PHONE é requerido")
+    protected String phone;
+
+    @NotEmpty(message = "Campo E-MAIL é requerido")
+    @Email
+    protected String email;
+
+    @NotEmpty(message = "Campo SENHA é requerido")
+    @Size(min = 3, max = 99, message = "O campo SENHA deve ter entre 3 e 99 caracteres")
+    protected String senha;
+
+    @Size(max = 3000, message = "O campo SOBRE pode ter entre 0 e 3000 caracteres")
+    protected String sobre;
+
+    protected Set<Integer> perfis;
+    protected LocalDateTime dataCriacao;
+
+}
