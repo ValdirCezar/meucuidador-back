6eca0334578cdac37946bfc0208a58bef8b4d934
Valdir Cezar <valdircezar312@gmail.com>
2021-09-16 18:45:49 -0300

    feat: exceção para cpf, email ou telefone já cadastrado

--- src/main/java/com/valdir/meucuidador/repository/CuidadorRepository.java
+++ src/main/java/com/valdir/meucuidador/repository/CuidadorRepository.java
@@ -3,5 +3,11 @@ package com.valdir.meucuidador.repository;
 import com.valdir.meucuidador.domain.Cuidador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CuidadorRepository extends JpaRepository<Cuidador, Integer> {
+
+    Optional<Cuidador> findByCpf(String cpf);
+    Optional<Cuidador> findByEmail(String email);
+    Optional<Cuidador> findByPhone(String phone);
 }
---
+++ src/main/java/com/valdir/meucuidador/services/exception/DataIntegratyViolationException.java
@@ -0,0 +1,8 @@
+package com.valdir.meucuidador.services.exception;
+
+public class DataIntegratyViolationException extends RuntimeException{
+
+    public DataIntegratyViolationException(String message) {
+        super(message);
+    }
+}
