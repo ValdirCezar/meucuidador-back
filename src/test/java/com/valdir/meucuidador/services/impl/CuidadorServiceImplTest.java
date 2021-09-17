3dda10d7159488f8fb48697b0956ae47739fbc95
Valdir Cezar <valdircezar312@gmail.com>
2021-09-17 10:59:00 -0300

    feat: melhorias nos testes para cuidador

--- src/test/java/com/valdir/meucuidador/services/impl/CuidadorServiceImplTest.java
+++ src/test/java/com/valdir/meucuidador/services/impl/CuidadorServiceImplTest.java
@@ -6,7 +6,6 @@ import com.valdir.meucuidador.domain.enums.Perfil;
 import com.valdir.meucuidador.repository.CuidadorRepository;
 import com.valdir.meucuidador.services.exception.DataIntegratyViolationException;
 import com.valdir.meucuidador.services.exception.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -16,6 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
 
 @SpringBootTest
 class CuidadorServiceImplTest {
@@ -30,8 +34,6 @@ class CuidadorServiceImplTest {
     private static final String SOBRE      = "Sobre";
     private static final String SENHA      = "123";
     private static final String PHONE      = "43984634308";
-
-    private static final String JA_CADASTRADO_NO_SISTEMA = "já cadastrado no sistema";
 
     @InjectMocks
     private CuidadorServiceImpl service;
@@ -52,98 +54,94 @@ class CuidadorServiceImplTest {
 
     @Test
     void findByIdSuccessTest() {
-        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(this.optionalCuidador);
+
+        when(repository.findById(anyInt())).thenReturn(this.optionalCuidador);
 
         Cuidador response = service.findById(ID);
 
         if(optionalCuidador.isPresent()) {
-            Assertions.assertEquals(optionalCuidador.get().getId(), response.getId());
-            Assertions.assertEquals(optionalCuidador.get().getNome(), response.getNome());
-            Assertions.assertEquals(optionalCuidador.get().getCpf(), response.getCpf());
-            Assertions.assertEquals(optionalCuidador.get().getEmail(), response.getEmail());
-            Assertions.assertEquals(optionalCuidador.get().getPhone(), response.getPhone());
-            Assertions.assertEquals(optionalCuidador.get().getSenha(), response.getSenha());
-            Assertions.assertEquals(optionalCuidador.get().getSobre(), response.getSobre());
-            Assertions.assertEquals(optionalCuidador.get().getPerfis(), response.getPerfis());
+            assertEquals(optionalCuidador.get().getId(), response.getId());
+            assertEquals(optionalCuidador.get().getNome(), response.getNome());
+            assertEquals(optionalCuidador.get().getCpf(), response.getCpf());
+            assertEquals(optionalCuidador.get().getEmail(), response.getEmail());
+            assertEquals(optionalCuidador.get().getPhone(), response.getPhone());
+            assertEquals(optionalCuidador.get().getSenha(), response.getSenha());
+            assertEquals(optionalCuidador.get().getSobre(), response.getSobre());
+            assertEquals(optionalCuidador.get().getPerfis(), response.getPerfis());
         }
     }
 
     @Test
     void findByIdErrorTest() {
-        ObjectNotFoundException notFoundException = new ObjectNotFoundException("Objeto não encontrado");
-        Mockito.when(repository.findById(Mockito.anyInt())).thenThrow(notFoundException);
 
         try{
             service.findById(ID);
         } catch (Exception ex) {
-            Assertions.assertEquals(notFoundException.getClass(), ex.getClass());
-            Assertions.assertEquals(notFoundException.getMessage(), ex.getMessage());
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
         }
+
     }
 
     @Test
     void findAllTest() {
         List<Cuidador> list = List.of(cuidador);
 
-        Mockito.when(repository.findAll()).thenReturn(list);
+        when(repository.findAll()).thenReturn(list);
         List<Cuidador> response = service.findAll();
 
-        Assertions.assertEquals(response.size(), list.size());
-        Assertions.assertEquals(response.get(INDEX_0).getId(), list.get(INDEX_0).getId());
-        Assertions.assertEquals(response.get(INDEX_0).getNome(), list.get(INDEX_0).getNome());
-        Assertions.assertEquals(response.get(INDEX_0).getCpf(), list.get(INDEX_0).getCpf());
-        Assertions.assertEquals(response.get(INDEX_0).getEmail(), list.get(INDEX_0).getEmail());
-        Assertions.assertEquals(response.get(INDEX_0).getPhone(), list.get(INDEX_0).getPhone());
-        Assertions.assertEquals(response.get(INDEX_0).getSenha(), list.get(INDEX_0).getSenha());
-        Assertions.assertEquals(response.get(INDEX_0).getSobre(), list.get(INDEX_0).getSobre());
-        Assertions.assertEquals(response.get(INDEX_0).getPerfis(), list.get(INDEX_0).getPerfis());
+        assertEquals(response.size(), list.size());
+        assertEquals(response.get(INDEX_0).getId(), list.get(INDEX_0).getId());
+        assertEquals(response.get(INDEX_0).getNome(), list.get(INDEX_0).getNome());
+        assertEquals(response.get(INDEX_0).getCpf(), list.get(INDEX_0).getCpf());
+        assertEquals(response.get(INDEX_0).getEmail(), list.get(INDEX_0).getEmail());
+        assertEquals(response.get(INDEX_0).getPhone(), list.get(INDEX_0).getPhone());
+        assertEquals(response.get(INDEX_0).getSenha(), list.get(INDEX_0).getSenha());
+        assertEquals(response.get(INDEX_0).getSobre(), list.get(INDEX_0).getSobre());
+        assertEquals(response.get(INDEX_0).getPerfis(), list.get(INDEX_0).getPerfis());
     }
 
     @Test
     void createSuccessTest() {
-        Mockito.when(repository.save(Mockito.any(Cuidador.class))).thenReturn(cuidador);
+
+        when(repository.save(Mockito.any(Cuidador.class))).thenReturn(cuidador);
 
         Cuidador response = service.create(cuidadorDTO);
 
-        Assertions.assertEquals(cuidador.getId(), response.getId());
-        Assertions.assertEquals(cuidador.getNome(), response.getNome());
-        Assertions.assertEquals(cuidador.getCpf(), response.getCpf());
-        Assertions.assertEquals(cuidador.getEmail(), response.getEmail());
-        Assertions.assertEquals(cuidador.getPhone(), response.getPhone());
-        Assertions.assertEquals(cuidador.getSenha(), response.getSenha());
-        Assertions.assertEquals(cuidador.getSobre(), response.getSobre());
-        Assertions.assertEquals(cuidador.getPerfis(), response.getPerfis());
+        assertEquals(cuidador.getId(), response.getId());
+        assertEquals(cuidador.getNome(), response.getNome());
+        assertEquals(cuidador.getCpf(), response.getCpf());
+        assertEquals(cuidador.getEmail(), response.getEmail());
+        assertEquals(cuidador.getPhone(), response.getPhone());
+        assertEquals(cuidador.getSenha(), response.getSenha());
+        assertEquals(cuidador.getSobre(), response.getSobre());
+        assertEquals(cuidador.getPerfis(), response.getPerfis());
     }
     
 
     @Test
     void createWithCPFErrorTest() {
-        DataIntegratyViolationException exception
-                = new DataIntegratyViolationException("CPF " + JA_CADASTRADO_NO_SISTEMA);
 
-        Mockito.when(repository.findByCpf(Mockito.any())).thenReturn(optionalCuidador);
+        when(repository.findByCpf(any())).thenReturn(optionalCuidador);
 
         try{
             cuidadorDTO.setId(ID_2);
             service.create(cuidadorDTO);
         } catch (Exception ex) {
-            Assertions.assertEquals(exception.getClass(), ex.getClass());
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
         }
 
     }
 
     @Test
     void createWithEmailErrorTest() {
-        DataIntegratyViolationException exception
-                = new DataIntegratyViolationException("E-MAIL " + JA_CADASTRADO_NO_SISTEMA);
 
-        Mockito.when(repository.findByEmail(Mockito.any())).thenReturn(optionalCuidador);
+        when(repository.findByEmail(any())).thenReturn(optionalCuidador);
 
         try{
             cuidadorDTO.setId(ID_2);
             service.create(cuidadorDTO);
         } catch (Exception ex) {
-            Assertions.assertEquals(exception.getClass(), ex.getClass());
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
         }
 
     }
@@ -151,32 +149,33 @@ class CuidadorServiceImplTest {
     @Test
     void createWithPhoneErrorTest() {
 
-        Mockito.when(repository.findByPhone(Mockito.any())).thenReturn(optionalCuidador);
+        when(repository.findByPhone(any())).thenReturn(optionalCuidador);
 
         try{
             cuidadorDTO.setId(ID_2);
             service.create(cuidadorDTO);
         } catch (Exception ex) {
-            Assertions.assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
         }
 
     }
 
     @Test
     void updateWithSuccessTest() {
-        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optionalCuidador);
-        Mockito.when(repository.save(Mockito.any())).thenReturn(cuidador);
+
+        when(repository.findById(anyInt())).thenReturn(optionalCuidador);
+        when(repository.save(any())).thenReturn(cuidador);
 
         Cuidador response = service.update(cuidadorDTO, ID);
 
-        Assertions.assertEquals(cuidador.getId(), response.getId());
-        Assertions.assertEquals(cuidador.getNome(), response.getNome());
-        Assertions.assertEquals(cuidador.getCpf(), response.getCpf());
-        Assertions.assertEquals(cuidador.getEmail(), response.getEmail());
-        Assertions.assertEquals(cuidador.getPhone(), response.getPhone());
-        Assertions.assertEquals(cuidador.getSenha(), response.getSenha());
-        Assertions.assertEquals(cuidador.getSobre(), response.getSobre());
-        Assertions.assertEquals(cuidador.getPerfis(), response.getPerfis());
+        assertEquals(cuidador.getId(), response.getId());
+        assertEquals(cuidador.getNome(), response.getNome());
+        assertEquals(cuidador.getCpf(), response.getCpf());
+        assertEquals(cuidador.getEmail(), response.getEmail());
+        assertEquals(cuidador.getPhone(), response.getPhone());
+        assertEquals(cuidador.getSenha(), response.getSenha());
+        assertEquals(cuidador.getSobre(), response.getSobre());
+        assertEquals(cuidador.getPerfis(), response.getPerfis());
     }
 
     @Test
@@ -184,8 +183,8 @@ class CuidadorServiceImplTest {
         try {
             service.update(cuidadorDTO, ID);
         } catch (Exception ex) {
-            Assertions.assertEquals(ObjectNotFoundException.class, ex.getClass());
-            Assertions.assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado", ex.getMessage());
         }
     }
 
@@ -204,6 +203,7 @@ class CuidadorServiceImplTest {
 
     private void iniciaCuidador() {
         cuidador = new Cuidador();
+
         cuidador.setId(ID);
         cuidador.setNome(NOME);
         cuidador.setCpf(CPF);
